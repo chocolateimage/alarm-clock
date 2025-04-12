e7f2e83a92bdf72f0f05712d52a2556ca04b004f
chocolateimage <chocolateimage@protonmail.com>
2025-04-12 17:03:20 +0200

    Bump version to v1.1.0

--- alarm-clock.py
+++ alarm-clock.py
@@ -55,7 +55,7 @@ class Application(QApplication):
         super().__init__(argv)
         self.setApplicationName("alarm-clock")
         self.setApplicationDisplayName("Alarm Clock")
-        self.setApplicationVersion("1.0.0")
+        self.setApplicationVersion("1.1.0")
         if os.environ.get("ALARMCLOCK_DEBUG", "0") != "1":
             self.setQuitOnLastWindowClosed(False)
 
