ddb9a3b235970c91775818de424b125a5027cd41
chocolateimage <chocolateimage@protonmail.com>
2025-05-24 22:57:27 +0200

    Add .desktop file

--- build.sh
+++ build.sh
@@ -11,5 +11,7 @@ mkdir -p "$DIRECTORY_NAME/DEBIAN"
 cp ../control "$DIRECTORY_NAME/DEBIAN/"
 mkdir -p "$DIRECTORY_NAME/usr/bin"
 cp ../alarm-clock.py "$DIRECTORY_NAME/usr/bin/alarm-clock"
+mkdir -p "$DIRECTORY_NAME/usr/share/applications"
+cp ../alarm-clock.desktop "$DIRECTORY_NAME/usr/share/applications/"
 
 dpkg-deb --build "$DIRECTORY_NAME"
